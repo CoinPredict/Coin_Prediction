23ba9655ffc3c9031639d57e9a3081b2a6035f76
monlaoi002 <94620492+monlaoi002@users.noreply.github.com>
2024-10-01 19:13:50 +0900

    m

--- 8.py
+++ 8.py
@@ -9,6 +9,6 @@ data.set_index('Date', inplace=True)
 
 mpf.plot(data, type='candle', volume=True, 
          title='Tesla Stock Candlestick Chart', 
-         ylabel='Price (USD)', 
+         ylabel='Price USD', 
          ylabel_lower='Volume', 
-         figsize=(14, 7))  # 그래프 크기 조정
+         figsize=(14, 7))
